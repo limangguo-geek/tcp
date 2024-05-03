945d3a700c421339e1fce0f56feac3ff82d5d5f2
limangguo-geek <167859081+limangguo-geek@users.noreply.github.com>
2024-05-03 10:54:55 +0200

    Create tcp.sh

---
+++ tcp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 覆盖写入内容到 sysctl.conf
+cat <<EOF > /etc/sysctl.conf
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_rfc1337=0
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_moderate_rcvbuf=1
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
+net.ipv4.ip_local_port_range=1024 65535
+net.ipv4.tcp_rmem=4096 87380 33554432
+net.ipv4.tcp_wmem=4096 16384 33554432
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+net.ipv4.ip_forward=1
+fs.file-max=1000000
+EOF
+
+# 应用更改
+sysctl -p
